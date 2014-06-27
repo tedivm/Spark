29e5c2d0d602e90bde875b6fe877215b3a8230d5
Robert Hafner <tedivm@tedivm.com>
2014-06-27 11:38:53 -0700

    Setup initial testsuite

---
+++ tests/Spark/Test/ResourcesTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of the Stash package.
+ *
+ * (c) Robert Hafner <tedivm@tedivm.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Spark\Test;
+
+use Spark\Resources;
+
+class ResourcesTest extends \PHPUnit_Framework_TestCase
+{
+    protected $types = array('templates', 'config');
+
+    public function testGetPath()
+    {
+        $resources = new Resources();
+
+        foreach($this->types as $type) {
+
+            $path = $resources->getPath($type);
+            $this->assertTrue(file_exists($path), 'Returns valid path.');
+            $this->assertTrue(is_dir($path), 'Returns directory.');
+        }
+
+    }
+
+}
