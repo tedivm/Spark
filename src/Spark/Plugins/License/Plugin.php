352d8fcadd1f0aaa071ab33dd9af1ec39e90136b
Robert Hafner <tedivm@tedivm.com>
2014-06-28 17:26:50 -0700

    Added additional licenses

--- src/Spark/Plugins/License/Plugin.php
+++ src/Spark/Plugins/License/Plugin.php
@@ -7,11 +7,45 @@ use Symfony\Component\Console\Input\InputInterface;
 
 class Plugin extends Base
 {
+    protected $licenseAliases = array(
+        'agpl' => 'AGPL-3.0',
+
+        'apache' => 'Apache-2.0',
+
+        'artistic' => 'Artistic-2.0',
+
+        'bsd2' => 'BSD-2-Clause',
+        'bsd3' => 'BSD-3-Clause',
+
+        'cc0' => 'cc0',
+
+        'gpl' => 'GPL-2.0',
+        'gpl3' => 'GPL-3.0',
+
+        'isc' => 'isc',
+
+        'lgpl' => 'LGPL-2.1',
+        'lgpl3' => 'LGPL-3.0',
+
+        'mit' => 'MIT',
+
+        'mpl' => 'MPL-2.0',
+
+        'unlicense' => 'UNLICENSE',
+
+        'reserved' => 'All-Rights-Reserved',
+        'proprietary' => 'All-Rights-Reserved'
+    );
+
     public function getTemplateFiles(InputInterface $input)
     {
         $license = $input->getOption('license');
         if ($license == false || $license == 'none') {
             return array();
+        }
+
+        if ($license == 'unlicense') {
+            return array('files' => array('UNLICENSE'));
         }
 
         return array('files' => array('LICENSE'));
@@ -21,6 +55,11 @@ class Plugin extends Base
     {
         $license = $input->getOption('license');
         if ($license !== false && $license !== 'none') {
+
+            if (isset($this->licenseAliases[$license])) {
+                $license = $this->licenseAliases[$license];
+            }
+
             $tags['license'] = $license;
         }
     }
